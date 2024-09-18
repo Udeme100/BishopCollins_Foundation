1b1d0cc50286c28cae16c63019a633bab5f9c322
Udeme100 <udemx200@gmail.com>
2024-09-18 02:47:50 +0100

    First Commit

---
+++ mobile.js
@@ -0,0 +1,4 @@
+document.getElementById('navToggle').addEventListener('click', function() {
+    const navbar = document.querySelector('.navbar-nav');
+    navbar.classList.toggle('show-nav');
+});
---
+++ slider.js
@@ -0,0 +1,10 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function showNextSlide() {
+    slides[currentSlide].classList.remove('active');
+    currentSlide = (currentSlide + 1) % slides.length;
+    slides[currentSlide].classList.add('active');
+}
+
+setInterval(showNextSlide, 4000); // Change slide every 4 seconds
